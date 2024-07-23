a3520a0acd61e6c06bcdf652f323ed9ee6c2c46c
ndy <ratcomp9992@gmail.com>
2024-07-24 07:53:25 +0900

    Init: initial project

---
+++ src/main/java/Soodgarak/Soodgarak/SoodgarakApplication.java
@@ -0,0 +1,13 @@
+package Soodgarak.Soodgarak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoodgarakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoodgarakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Soodgarak/Soodgarak/SoodgarakApplicationTests.java
@@ -0,0 +1,13 @@
+package Soodgarak.Soodgarak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoodgarakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
