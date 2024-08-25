cba3feeeff773bb2029f8810e85e873e0dd0ca22
ndy <ratcomp9992@gmail.com>
2024-08-25 19:32:55 +0900

    Config(#9): Swagger 설정

---
+++ src/main/java/Soodgarak/Soodgarak/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package Soodgarak.Soodgarak.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Soodgarak") // API의 제목
+                .description("남은 식재료 기반 음식 추천 서비스. 근데 이제 MBTI를 곁들인"); // API에 대한 설명
+    }
+}
