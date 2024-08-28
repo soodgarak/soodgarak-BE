a8bd7166ee63c5ed97fde5677797d332e914d451
ndy <ratcomp9992@gmail.com>
2024-08-28 23:20:58 +0900

    Feat(#14): RecipeRequest 생성

--- src/main/java/Soodgarak/Soodgarak/domain/recipe/controller/model/RecipeRequest.java
+++ src/main/java/Soodgarak/Soodgarak/domain/recipe/controller/model/RecipeRequest.java
@@ -1,11 +1,8 @@
 package Soodgarak.Soodgarak.domain.recipe.controller.model;
 
-import lombok.Getter;
-
-@Getter
 public record RecipeRequest(
         String category,
         String keyword,
-        Integer index
+        Integer page
 ) {
 }
