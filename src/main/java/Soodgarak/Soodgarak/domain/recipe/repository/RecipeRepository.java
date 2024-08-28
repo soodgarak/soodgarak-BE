eff0016e0229724d7c693ae492b1d508ac6fa7a6
ndy <ratcomp9992@gmail.com>
2024-08-28 18:23:13 +0900

    Feat(#14): 요청 값에 해당하는 데이터 갯수 조회 기능 구현

--- src/main/java/Soodgarak/Soodgarak/domain/recipe/repository/RecipeRepository.java
+++ src/main/java/Soodgarak/Soodgarak/domain/recipe/repository/RecipeRepository.java
@@ -2,6 +2,12 @@ package Soodgarak.Soodgarak.domain.recipe.repository;
 
 import Soodgarak.Soodgarak.domain.recipe.domain.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    Long countByMbtiStartingWith(String keyword);
+    Long countByMbtiEndingWith(String keyword);
+    Long countByMenuContaining(String keyword);
+    Long countByIngredientContaining(String keyword);
 }
--- src/main/java/Soodgarak/Soodgarak/domain/recipe/service/RecipeService.java
+++ src/main/java/Soodgarak/Soodgarak/domain/recipe/service/RecipeService.java
@@ -1,7 +1,23 @@
 package Soodgarak.Soodgarak.domain.recipe.service;
 
+import Soodgarak.Soodgarak.domain.recipe.repository.RecipeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RecipeService {
+    private final RecipeRepository recipeRepository;
+
+    private Long countData(String keyword) {
+        if (keyword.isBlank()) {
+            return recipeRepository.count();
+        } else if (keyword.equals("M") || keyword.equals("S") || keyword.equals("V")) {
+            return recipeRepository.countByMbtiStartingWith(keyword);
+        } else if (keyword.equals("H") || keyword.equals("N")) {
+            return recipeRepository.countByMbtiEndingWith(keyword);
+        } else {
+            return recipeRepository.countByMenuContaining(keyword) + recipeRepository.countByIngredientContaining(keyword);
+        }
+    }
 }
