f5b3f3e811f5a14c8abe588099350c6c387c5e2f
ndy <ratcomp9992@gmail.com>
2024-08-29 00:07:01 +0900

    Chore(#14): 미사용 메서드 삭제

--- src/main/java/Soodgarak/Soodgarak/domain/recipe/service/RecipeService.java
+++ src/main/java/Soodgarak/Soodgarak/domain/recipe/service/RecipeService.java
@@ -20,24 +20,11 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class RecipeService {
-    private final RecipeRepository recipeRepository;
     private final RecipeQueryRepository recipeQueryRepository;
     private final RecipeRedis recipeRedis;
     private final CategoryRecipeRedis categoryRecipeRedis;
     private final SearchRecipeRedis searchRecipeRedis;
     private final Long initCheckValue = 0L;
-
-    private Long countData(String keyword) {
-        if (keyword.isBlank()) {
-            return recipeRepository.count();
-        } else if (keyword.equals("M") || keyword.equals("S") || keyword.equals("V")) {
-            return recipeRepository.countByMbtiStartingWith(keyword);
-        } else if (keyword.equals("H") || keyword.equals("N")) {
-            return recipeRepository.countByMbtiEndingWith(keyword);
-        } else {
-            return recipeRepository.countByMenuContaining(keyword) + recipeRepository.countByIngredientContaining(keyword);
-        }
-    }
 
     private void initRedis(RecipeGroup group) {
         if (group.equals(RecipeGroup.ALL) && recipeRedis.existsById(initCheckValue)) {
