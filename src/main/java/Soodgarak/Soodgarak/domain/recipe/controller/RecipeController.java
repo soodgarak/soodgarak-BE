8f7a71ac03c81a04f7f860bcad54ab9687192d58
ndy <ratcomp9992@gmail.com>
2024-08-28 23:21:17 +0900

    Feat(#14): Controller 생성

--- src/main/java/Soodgarak/Soodgarak/domain/recipe/controller/RecipeController.java
+++ src/main/java/Soodgarak/Soodgarak/domain/recipe/controller/RecipeController.java
@@ -1,13 +1,44 @@
 package Soodgarak.Soodgarak.domain.recipe.controller;
 
+import Soodgarak.Soodgarak.domain.recipe.controller.model.RecipeRequest;
+import Soodgarak.Soodgarak.domain.recipe.domain.Recipe;
+import Soodgarak.Soodgarak.domain.recipe.service.RecipeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Recipe", description = "레시피 조회 API")
 @RequestMapping("/recipe")
 public class RecipeController {
+    private final RecipeService recipeService;
 
+    @GetMapping
+    @Operation(summary = "레시피 조회", description = "조건에 해당하는 레시피 List를 조회합니다.")
+    public ResponseEntity<List<Recipe>> getRecipeList(@ModelAttribute RecipeRequest recipeRequest) {
+        // 초기 30개
+        if (recipeRequest.page() == null) {
+            if (recipeRequest.keyword() != null) {
+
+            } else if (recipeRequest.category() != null) {
+
+            } else {
+                return ResponseEntity.ok(recipeService.getAllRecipeList());
+            }
+        } else {
+            if (recipeRequest.keyword() != null) {
+
+            } else if (recipeRequest.category() != null) {
+
+            } else {
+
+            }
+        }
+        return ResponseEntity.ok(null);
+    }
 }
