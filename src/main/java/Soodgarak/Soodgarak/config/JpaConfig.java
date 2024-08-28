f9e58742e355cb9513c6d9d07cbb964fa3347d70
ndy <ratcomp9992@gmail.com>
2024-08-29 00:09:27 +0900

    Config(#14): JpaConfig 추가

---
+++ src/main/java/Soodgarak/Soodgarak/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package Soodgarak.Soodgarak.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
