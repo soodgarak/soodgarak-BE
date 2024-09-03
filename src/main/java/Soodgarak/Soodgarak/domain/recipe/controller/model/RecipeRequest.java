031652da63ed890af18566d878dd673b5fca4e77
ndy <ratcomp9992@gmail.com>
2024-09-03 12:27:07 +0900

    Chore(#14): category, keyword 대소문자 구분 로직 제거

--- src/main/java/Soodgarak/Soodgarak/domain/recipe/controller/model/RecipeRequest.java
+++ src/main/java/Soodgarak/Soodgarak/domain/recipe/controller/model/RecipeRequest.java
@@ -7,8 +7,8 @@ public record RecipeRequest(
         Integer page
 ) {
     public RecipeRequest(String category, String keyword, String mbti, Integer page) {
-        this.category = category != null ? category.toLowerCase() : null;
-        this.keyword = keyword != null ? keyword.toLowerCase() : null;
+        this.category = category;
+        this.keyword = keyword;
         this.mbti = mbti != null ? mbti.toUpperCase() : null;
         this.page = page;
     }
