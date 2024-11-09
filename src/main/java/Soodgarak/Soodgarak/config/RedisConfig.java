8d9f92796839af76023a5c0d75b86051b39dc3d9
ndy <ratcomp9992@gmail.com>
2024-11-09 10:44:59 +0900

    Config(#34): RedisTemplate Config 추가

--- src/main/java/Soodgarak/Soodgarak/config/RedisConfig.java
+++ src/main/java/Soodgarak/Soodgarak/config/RedisConfig.java
@@ -3,10 +3,13 @@ package Soodgarak.Soodgarak.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -25,5 +28,15 @@ public class RedisConfig {
         config.setPassword(RedisPassword.of(redisPassword));
 
         return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
     }
 }
